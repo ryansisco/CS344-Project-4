362d551ec3a52857360c8d7562701372934f829f
Ryan Sisco <ryansisco8@gmail.com>
2018-03-15 16:48:53 -0700

    finished keygen

--- keygen.c
+++ keygen.c
@@ -1,8 +1,35 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <time.h>
 
-int main() {
-
+int main(int argc, char **argv) {
+	if (argc != 2) {	//Not 2 command line args
+		printf("ERROR\n");
+		printf("SYNTAX: keygen [key length]\n");
+		exit(1);
+	}
+	else {	// 2 command line args
+		srand(time(NULL));
+		int i, keylength = atoi(argv[1]);
+		if (keylength == 0) {
+			printf("ERROR\n");
+			printf("SYNTAX: keygen [key length]\n");
+			exit(1);
+		}
+		else {
+			for (i = 0; i < keylength; i++) {
+				char key = (rand() % (91 - 65 + 1)) + 65;
+				// ascii capital 65-90 + '['
+				if (key == '[') {	// changes '[' to ' '
+					printf(" ");
+				}
+				else {	// prints key if not '['
+					printf("%c", key);
+				}
+			}
+			printf("\n");
+		}
+	}
 	return 0;
 }
